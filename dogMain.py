18d9037cb955fbfc5d76e9a5d4aad89859100f34
brettjm <brettjmanning@gmail.com>
2018-10-13 12:14:21 -0500

    Add delay after button press

--- dogMain.py
+++ dogMain.py
@@ -7,14 +7,14 @@ PIN_POTTY = 8
 
 ACCOUNT_SID   = 'AC06d136e738e32531a3c1f14bf50adf20'
 AUTHTOKEN     = 'cbe0e851ddff3d88f6426bafa044b7c8'
-# MY_NUMBER     = '+18015084976'
-MY_NUMBER     = '+19728387046'
+MY_NUMBER     = '+18015084976'  # Brett's number
+#MY_NUMBER     = '+19728387046'  # Sarah's number
 TWILIO_NUMBER = '+13854744945'
 
 def setup():
    GPIO.setmode(GPIO.BCM)
-   # GPIO.setup(PIN_FOOD, GPIO.IN)
-   GPIO.setup(PIN_FOOD, GPIO.IN, GPIO.PUD_DOWN)
+   GPIO.setup(PIN_FOOD,  GPIO.IN, GPIO.PUD_DOWN)
+   GPIO.setup(PIN_POTTY, GPIO.IN, GPIO.PUD_DOWN)
 
 def sendAlert(message):
    twilioClient = Client(ACCOUNT_SID, AUTHTOKEN)
@@ -27,11 +27,17 @@ def main():
    # normally 0, when connected 1
    try:
       while(True):
-         print(GPIO.input(PIN_FOOD))
-         time.sleep(.1)
+         if (GPIO.input(PIN_FOOD)):
+            print("Food")
+            time.sleep(2)
+
+         if (GPIO.input(PIN_POTTY)):
+            print("Potty")
+            time.sleep(2)
+   
    except KeyboardInterrupt:
       GPIO.cleanup()
-      print("\nExiting")
+      print("Exiting")
 
    # sendAlert('Feed me please!')
 
